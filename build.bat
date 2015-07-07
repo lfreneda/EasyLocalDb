2ad1af30b0d98231a77b02bb7d8655244a0bb784
Luiz Freneda <lfreneda@gmail.com>
2015-07-07 14:55:17 -0300

    Nuget package

---
+++ build.bat
@@ -0,0 +1,3 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild src\EasyLocalDb\EasyLocalDb.csproj /p:Configuration=Release
+.nuget\NuGet.exe pack src\EasyLocalDb\EasyLocalDb.csproj -Prop Configuration=Release
+
